4aa0dd485a9c8bf101caa8e07c5f40488f606639
sasha-chan <come@me.bro>
2015-01-19 00:46:55 -0500

    Okay here we go

--- Main.cpp
+++ Main.cpp
@@ -7,26 +7,21 @@
 #include "Software.h"
 
 //product constructor format is: name, price
-Product products[] = {
-	NWPC("NW-PC", 675.00),
-	Product("Memory Card", 69.95),
-	Product("Disk Drive", 198.50),
-	Software("Software", 34.98),
+Product* products[] = {
+	new NWPC("NW-PC", 675.00),
+	new Product("Memory Card", 69.95),
+	new Product("Disk Drive", 198.50),
+	new Software("Software", 34.98),
 };
 
 int main()
 {
 	printf("Starting Sasha's debug thing.\n");
 
-	NWPC thing1("NW-PC", 675.00);
-	Product thing2("Memory Card", 69.95);
-	Product thing3("Disk Drive", 198.50);
-	Software thing4("Software", 34.98);
-
-	thing1.getPriceForQuantity(0);
-	thing2.getPriceForQuantity(0);
-	thing3.getPriceForQuantity(0);
-	thing4.getPriceForQuantity(0);
+	products[0]->getPriceForQuantity(0);
+	products[1]->getPriceForQuantity(0);
+	products[2]->getPriceForQuantity(0);
+	products[3]->getPriceForQuantity(0);
 
 	printf("Ending Sasha's debug thing.\n");
 
@@ -38,14 +33,14 @@ int main()
 	getline(std::cin, customerName);
 
 	//add products to cart
-	for (Product product : products) {
-		std::cout << "Quantity of " + product.getName() + ": ";
+	for (Product* product : products) {
+		std::cout << "Quantity of " + product->getName() + ": ";
 
 		int quantity;
 		std::cin >> quantity;
 
 		if (quantity > 0) {
-			cart.addToCart(product, quantity);
+			cart.addToCart(*product, quantity);
 		}
 	}
 
